a26fcd34240c4f3041006c4a9840c0101f05a9d6
Skaara9111 <skaara911@gmail.com>
2021-12-05 19:55:40 +0300

    Добавлены возможность установить время начала уведомлений возможность отмены уведомлений переход в активити после нажатия на уведомление
    Уведомления приходят каждые 15 минут до отмены

--- app/src/main/java/com/example/straightenup/MainActivity.kt
+++ app/src/main/java/com/example/straightenup/MainActivity.kt
@@ -1,16 +1,22 @@
 package com.example.straightenup
 
+import android.app.AlarmManager
 import android.app.NotificationChannel
 import android.app.NotificationManager
-import android.content.Context
+import android.app.PendingIntent
 import android.content.Intent
-import android.graphics.Color
 import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.DatePicker
+import android.widget.Toast
 import androidx.core.app.NotificationCompat
 import androidx.core.app.NotificationManagerCompat
+import com.example.straightenup.databinding.ActivityMainBinding
+import com.google.android.material.timepicker.MaterialTimePicker
+import com.google.android.material.timepicker.TimeFormat
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -22,26 +28,89 @@ class MainActivity : AppCompatActivity() {
         const val description = "This is my channel";
     }
 
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var alarmManager: AlarmManager
+    private lateinit var pendingIntent: PendingIntent
+    private lateinit var picker: MaterialTimePicker
+    private lateinit var calendar: Calendar
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
         createNotificationChannel()
 
-        val button: Button = findViewById(R.id.button)
-        button.setOnClickListener {
-            var builder = NotificationCompat.Builder(this, CHANNEL_ID)
-                .setSmallIcon(R.drawable.ic_launcher_background)
-                .setContentTitle("My notification")
-                .setContentText("Straighten Up")
-                .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+        binding.selectTimeButton.setOnClickListener {
+            showTimePicker()
+        }
+        binding.setAlarmButton.setOnClickListener {
+            setAlarm()
+        }
+        binding.cancelAlarmButton.setOnClickListener {
 
-            with(NotificationManagerCompat.from(this)) {
-                notify(NOTIFICATION_ID, builder.build()) // посылаем уведомление
+            cancelAlarm()
+        }
+    }
+
+    private fun showTimePicker() {
+        picker = MaterialTimePicker.Builder()
+            .setTimeFormat(TimeFormat.CLOCK_24H)
+            .setHour(12)
+            .setMinute(0)
+            .setTitleText("Select Alarm Time")
+            .build()
+        picker.show(supportFragmentManager, "reded")
+
+        picker.addOnPositiveButtonClickListener {
+            if (picker.hour > 12) {
+                binding.selectedTime.text =
+                String.format("%02d", picker.hour - 12) + " : " + String.format(
+                    "%02d",
+                    picker.minute
+                ) + "PM"
+            } else {
+                String.format("%02d", picker.hour) + " : " + String.format(
+                    "%02d",
+                    picker.minute
+                ) + "AM"
             }
+
+            calendar = Calendar.getInstance()
+            calendar[Calendar.HOUR_OF_DAY] = picker.hour
+            calendar[Calendar.MINUTE] = picker.minute
+            calendar[Calendar.SECOND] = 0
+            calendar[Calendar.MILLISECOND] = 0
         }
 
+
+    }
+
+    private fun setAlarm() {
+        alarmManager = getSystemService(ALARM_SERVICE) as AlarmManager
+        val intent = Intent(this, AlarmReceiver::class.java)
+
+        pendingIntent = PendingIntent.getBroadcast(this, 0, intent, 0)
+
+        alarmManager.setRepeating(
+            AlarmManager.RTC_WAKEUP,
+            calendar.timeInMillis,
+            AlarmManager.INTERVAL_FIFTEEN_MINUTES,
+            pendingIntent
+        )
+
+        Toast.makeText(this, "Alarm set Successfully", Toast.LENGTH_SHORT).show()
+    }
+
+    private fun cancelAlarm() {
+        alarmManager = getSystemService(ALARM_SERVICE) as AlarmManager
+        val intent = Intent(this, AlarmReceiver::class.java)
+
+        pendingIntent = PendingIntent.getBroadcast(this, 0, intent, 0)
+
+        alarmManager.cancel(pendingIntent)
+
+        Toast.makeText(this, "Alarm Cancelled", Toast.LENGTH_SHORT).show()
     }
 
     private fun createNotificationChannel() {
@@ -51,7 +120,7 @@ class MainActivity : AppCompatActivity() {
             // Create the NotificationChannel
             val name = channel_name
             val descriptionText = description
-            val importance = NotificationManager.IMPORTANCE_DEFAULT
+            val importance = NotificationManager.IMPORTANCE_HIGH
             val mChannel = NotificationChannel(CHANNEL_ID, name, importance)
             mChannel.description = descriptionText
             // Register the channel with the system; you can't change the importance
